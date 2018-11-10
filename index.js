69f2b464671afab33a4ca6d0f9bf456c91d7dba6
hosscine <futagamirentaro@gmail.com>
2018-11-11 02:48:22 +0900

    add R code to get the event data from the web page

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ let Botkit = require('botkit')
 let R = require("r-script")
 require("dotenv").config()
 
+let names = ["にゃんチケ☆チャンス！", "ゲリラ月曜ステージ", "", "ゲリラ経験値にゃ！",
+  "トレジャー☆フェスティバル（日本編）", "超ゲリラ経験値にゃ！", "レンガと宇宙石の洞窟",
+  "備長炭と謎の骨の島", "羽根と歯車と黄金の岬", "トレジャー☆フェスティバル（未来編）",
+  "ゲリラ火曜ステージ", "ゲリラ水曜ステージ", "ゲリラ木曜ステージ", "ゲリラ金曜ステージ"]
+let check = [true, false, false, true,
+  false, false, false,
+  false, false, false,
+  false, false, false, false]
+
 // initialize
 let controller = Botkit.slackbot()
 let bot = controller.spawn({ token: process.env.DB_TOKEN })
@@ -14,19 +23,22 @@ let bot = controller.spawn({ token: process.env.DB_TOKEN })
 controller.hears(["hello"], ["direct_message", "direct_mention", "mention"],
   (bot, message) => bot.reply(message, 'hello! I am bot!!'))
 
-let notifDay = function() {
+let notifDay = function () {
   bot.say({
     channel: "bot-notification",
     text: "にゃんこ大戦争"
   })
 }
 
-let notifTime = function() {
-  let out = R("scraping.R").callSync()
-  bot.say({
-    channel: "bot-notification",
-    text: out
-  })
+let notifTime = function () {
+  let out = R("scraping.R")
+    .data(check)
+    .callSync()
+  for (let event of out)
+    bot.say({
+      channel: "bot-notification",
+      text: event.time + " " + names[event.name - 1]
+    })
 }
 
 notifTime()
--- scraping.R
+++ scraping.R
@@ -1,2 +1,23 @@
-needs(magrittr)
-1:4 %>% sum
+needs(tidyverse)
+needs(rvest)
+needs(lubridate)
+
+x <- read_html("https://ponos.s3.amazonaws.com/information/appli/battlecats/calendar/index.html")
+
+table <- x %>%
+  html_nodes("div.cld_box01") %>% 
+  html_node("table") %>% 
+  html_table() %>% 
+  imap_dfr(~ tibble(wday = rep(.y, nrow(.x[-1,])),
+                    time = .x[-1,]$X1,
+                    name = .x[-1,]$X2 %>% str_sub(2)))
+
+events <- table$name %>% str_split("・") %>% unlist() %>% unique()
+week.day <- today() %>% wday()
+
+check <- input[[1]] %>% unlist
+
+target.event <- table %>% filter(wday == week.day, name %in% events[check])
+target.event$time %<>% str_sub(1, -5) %>% paste0(":00~")
+target.event$name %<>% map(~ (. == events) %>% which) %>% as.numeric
+target.event
