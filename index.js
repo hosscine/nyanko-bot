90e2e28d2c1f5fd6335edba1cd27d636c5a3921a
hosscine <futagamirentaro@gmail.com>
2018-11-10 18:53:56 +0900

    refactoring

--- index.js
+++ index.js
@@ -4,17 +4,11 @@
 let Botkit = require('botkit')
 require("dotenv").config()
 
+// initialize
 let controller = Botkit.slackbot()
-let bot = controller.spawn({
-  token: process.env.DB_TOKEN
-}).startRTM(function (err, bot, payload) {
-  // 初期処理
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
-})
-controller.hears(["hello"], ["direct_message", "direct_mention", "mention"], function (bot, message) {
-  // キーワードに反応した処理
-  bot.reply(message, 'hello! I am bot!!')
-})
-console.log("start!!")
+let bot = controller.spawn({ token: process.env.DB_TOKEN })
+  .startRTM((err) => { if (err) throw new Error('Could not connect to Slack') })
+
+// hear the direct message
+controller.hears(["hello"], ["direct_message", "direct_mention", "mention"],
+  (bot, message) => bot.reply(message, 'hello! I am bot!!'))
