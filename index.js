85b834cee4b00eccc08fd8a63e3af8286d954aaa
hosscine <futagamirentaro@gmail.com>
2018-11-19 19:48:48 +0900

    use @slack/client module instead of botkit

--- index.js
+++ index.js
@@ -53,4 +53,22 @@ new CronJob("00 00 * * * *", () => {
 // hear the direct message
 controller.hears(["hello"], ["direct_message", "direct_mention", "mention"],
   (bot, message) => bot.reply(message, 'hello! I am bot!!'))
-  
+
+
+const { WebClient } = require('@slack/client');
+
+// An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
+const token = process.env.DB_TOKEN;
+
+const web = new WebClient(token);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = process.env.DB_CHANNEL;
+
+// See: https://api.slack.com/methods/chat.postMessage
+web.chat.postMessage({ channel: conversationId, text: 'Hello there', as_user: true })
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
