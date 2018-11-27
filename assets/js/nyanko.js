822a7cf8669ddb16840f730dee76b7023c7ba47d
hosscine <futagamirentaro@gmail.com>
2018-11-28 00:12:06 +0900

    fix require

--- assets/js/nyanko.js
+++ assets/js/nyanko.js
@@ -1,48 +1,55 @@
-let todayEvents = {}
-let getTodayEvents = function (mday, wday) {
-  let events = []
+let todayEvents
+let check = require("../json/check_list.json")
+let monthly = require("../json/monthly_guerrilla.json")
+let weekly = require("../json/weekly_guerrilla.json")
 
-  for (let event of weekly)
-    if (event.event[0] && event.wday === wday)
-      events = events.concat(parseWeek(event))
+module.exports = {
 
-  for (let event of monthly)
-    if (event.mday === mday)
-      events.push({
-        name: event.event,
-        time: event.time + " " +
-          (event.event === "逆襲のカバちゃん" ? "(1時間)" : "(12時間)")
-      })
-  return events
-}
-console.log("hoge")
-let readPolicy = function () {
-
-}
-
-let writePolicy = function() {
-
-}
-
-let getDayMessage = function(events, policy) {
+  getTodayEvents(mday, wday) {
+    let events = []
   
-}
+    for (let event of weekly)
+      if (event.event[0] && event.wday === wday)
+        events = events.concat(this.parseWeek(event))
+  
+    for (let event of monthly)
+      if (event.mday === mday)
+        events.push({
+          name: event.event,
+          time: event.time + " " +
+            (event.event === "逆襲のカバちゃん" ? "(1時間)" : "(12時間)")
+        })
+    return events
+  },
 
-let getHourMessage = function(hour, events, policy) {
+  readPolicy() {
+    return check
+  },
 
-}
+  writePolicy() {
 
-let parseWeek = function (event) {
-  if (event.event.length === 1)
-    return { name: event.event[0], time: event.time }
-  let events = []
-  for (let name of event.event)
-    events.push({ name: name, time: event.time })
-  return events
-}
+  },
 
-let formatDayMessage = function (events) {
-  let header = "今日のイベントはこんな感じにゃ\n"
-  let contents = events.reduce((a, e) => a + e.time + " " + e.name + "\n", "")
-  return header + contents
+  getDayMessage(events, policy) {
+    return this.formatDayMessage(events)
+  },
+
+  getHourMessage(hour, events, policy) {
+
+  },
+
+  parseWeek(event) {
+    if (event.event.length === 1)
+      return { name: event.event[0], time: event.time }
+    let events = []
+    for (let name of event.event)
+      events.push({ name: name, time: event.time })
+    return events
+  },
+
+  formatDayMessage(events) {
+    let header = "今日のイベントはこんな感じにゃ\n"
+    let contents = events.reduce((a, e) => a + e.time + " " + e.name + "\n", "")
+    return header + contents
+  }
 }
--- index.js
+++ index.js
@@ -2,44 +2,45 @@
 // forever start index.js
 
 let moment = require("moment")
-let CronJob = require("cron").CronJob
-let WebClient = require('@slack/client');
+let { CronJob } = require("cron")
+let { WebClient } = require('@slack/client')
+let nyanko = require("./assets/js/nyanko.js")
 require("dotenv").config()
-require("./assets/js/nyanko.js")
-
-let check = require("./assets/json/check_list.json")
-let monthly = require("./assets/json/monthly_guerrilla.json")
-let weekly = require("./assets/json/weekly_guerrilla.json")
 
 new CronJob("00 00 00 * * *", () => {
-  let now = moment()
-  let mday = parseInt(now.format("DD"))
-  let wday = parseInt(now.format("E"))
-  todayEvents = getTodayEvents(mday, wday)
-  let policy = getPolicy()
+  // initializeEvents()
+  initializeEvents()
+  let policy = nyanko.readPolicy()
 
-  let message = getDayMessage(todayEvents, eventPolicy)
+  let message = nyanko.getDayMessage(todayEvents, policy)
   post(message)
 }, null, true)
 
 new CronJob("00 00 * * * *", () => {
-  let now = moment()
-  let hour = parseInt(now.format("HH"))
-  let policy = getPolicy()
+  if (!todayEvents) initializeEvents()
+  let policy = nyanko.readPolicy()
+  let hour = parseInt(moment().format("HH"))
 
-  let message = getHourMessage(hour, todayEvents, eventPolicy)
+  let message = nyanko.getDayMessage(todayEvents, policy)
   post(message)
 }, null, true)
 
 // An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
 const web = new WebClient(process.env.DB_TOKEN)
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = process.env.DB_CHANNEL;
+const conversationId = process.env.DB_CHANNEL
 
 let post = function (message) {
   // See: https://api.slack.com/methods/chat.postMessage
-  web.chat.postMessage({ channel: conversationId, text: message, as_user: true })
+  web.chat.postMessage({ channel: conversationId, text: message })
     // `res` contains information about the posted message
     .then(res => console.log('Message sent: ', res.ts))
     .catch(console.error)
+}
+
+let initializeEvents = function() {
+  let now = moment()
+  let mday = parseInt(now.format("DD"))
+  let wday = parseInt(now.format("E"))
+  todayEvents = nyanko.getTodayEvents(mday, wday)
 }
